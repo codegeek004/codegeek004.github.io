c45fffeb95a6a879eff4b25b7833e1c9e398ad39
Yash Vaishnav <yashvaishnav1404@gmail.com>
2023-06-15 14:35:38 +0530

    some message

---
+++ git-tutorial/bro.py
@@ -0,0 +1,16 @@
+given_name = "William"
+middle_names = "Bradley"
+family_name = "Pitt"
+
+name_length= given_name + middle_names + family_name
+print(name_length)
+print(len(name_length))
+
+
+# Now we check to make sure that the name fits within the driving license character limit
+# Nothing you need to do here
+driving_license_character_limit = 28
+print(name_length <= driving_license_character_limit)
+print(type('yash'))
+print("My age is {}".format(18))
+
---
+++ git-tutorial/c.py
@@ -0,0 +1,11 @@
+import numpy as np
+from pyzbar.pyzbar import decode
+import cv2
+img=cv2.imread("qr1.png")
+code=decode(img)
+print(code)
+for barcode in decode(img):
+    print(barcode.data)
+    text=barcode.data.decode('utf-8')
+    print(text)
+    print(barcode.rect)
